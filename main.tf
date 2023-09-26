c7fd928abe72613a13348635a1ed616b20ed2af6
Ify Eleweke <omaleweks@gmail.com>
2023-09-26 18:04:52 +0000

    Refactored S3 bucket into a module

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
+terraform {
+  # Terraform Cloud
+  #cloud {
+  #  organization = "terraform-bootcamp-23"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  
+  # Providers
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
   }
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
