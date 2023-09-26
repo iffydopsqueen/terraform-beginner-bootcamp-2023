903bdc1e625ae7f77f0eb3aa871bb6ded32d8dab
Ify Eleweke <70924414+iffydopsqueen@users.noreply.github.com>
2023-09-26 13:48:58 -0400

    Removed the random provider  (#25)
    * Removed the random provider from the project
    
    * Added a documentation describing what I did in the branch

--- providers.tf
+++ providers.tf
@@ -9,10 +9,6 @@ terraform {
   
   # Providers
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
