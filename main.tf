3f9fb08f0c8d47e1d870fdfeff56be4014378123
Ify Eleweke <70924414+iffydopsqueen@users.noreply.github.com>
2023-10-09 16:21:39 -0400

    Implemented Custom Terraform Provider  (#44)
    * Set up the skeleton for custom terraform provider
    
    * Implemented terraform provider code block

--- main.tf
+++ main.tf
@@ -9,19 +9,30 @@ terraform {
   
   # Providers
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
+    # aws = {
+    #   source = "hashicorp/aws"
+    #   version = "5.16.2"
+    # }
+
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
     }
   }
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1"  # mock data 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"    # mock data
 }
+
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
